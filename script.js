f994f7e6915fe47e7dba695e4996801c291545fa
Nicholas A. Thompson <nikolaythompson@gmail.com>
2026-01-08 20:55:54 -0800

    Servings estimate

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const resSolA = document.getElementById('res-sol-a');
     const resSolB = document.getElementById('res-sol-b');
     const resWater = document.getElementById('res-water');
+    const resCans = document.getElementById('res-cans');
+    const resGlasses = document.getElementById('res-glasses');
 
     function updateCalc() {
         let liters = parseFloat(input.value);
@@ -16,12 +18,18 @@ document.addEventListener('DOMContentLoaded', () => {
         const sugar = 104 * liters;
         const solA = 1 * liters;
         const solB = 10 * liters;
+        
+        // yield
+        const cans = liters * (1000 / 355);
+        const glasses = liters * (1000 / 250);
 
         // results
         resSugar.textContent = (liters === 0) ? "--" : `${sugar.toFixed(1)} g`;
         resSolA.textContent = (liters === 0) ? "--" : `${solA.toFixed(1)} mL`;
         resSolB.textContent = (liters === 0) ? "--" : `${solB.toFixed(1)} mL`;
         resWater.textContent = (liters === 0) ? "--" : `Fill to ${liters} L`;
+        resCans.textContent = (liters === 0) ? "--" : `${cans.toFixed(1)}`;
+        resGlasses.textContent = (liters === 0) ? "--" : `${glasses.toFixed(1)}`;
     }
 
     updateCalc();
